ae0cfe97f3449b8ca238ab088e8d5b44625e7241
나모 <knh9534@naver.com>
2021-04-24 09:00:04 +0000

    변수 선언

---
+++ src/002.Variable/variable02.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+	int a = 127;
+	
+	// printf("a의 값은 %d 진수로 %o 입니다. \n", 8, a);
+	// printf("a의 값은 %d 진수로 %d 입니다. \n", 10, a);
+	// printf("a의 값은 %d 진수로 %x 입니다. \n", 16, a);
+	
+	// printf("%d의 %d 진수 표현은 %o 입니다. \n", a, 8, a);
+	// printf("%d의 %d 진수 표현은 %d 입니다. \n", a, 10, a);
+	// printf("%d의 %d 진수 표현은 %x 입니다. \n", a, 16, a);
+	
+	printf("%o 은 %d 의 %d진수 표현입니다. \n", a, a, 8);
+	printf("%d 은 %d 의 %d진수 표현입니다. \n", a, a, 10);
+	printf("%x 은 %d 의 %d진수 표현입니다. \n", a, a, 16);
+	
+	return 0;
+}
