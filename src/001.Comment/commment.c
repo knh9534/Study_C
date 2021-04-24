9fc2ffabb3697531775f506ae238cca88b2dc85c
나모 <knh9534@naver.com>
2021-04-24 07:45:46 +0000

    주석 실습 추가

---
+++ src/001.Comment/commment.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+	/*
+	주석이 들어가는 부분
+	여러줄
+	가
+	나
+	다
+	*/
+	
+	//한줄 주석	
+	// printf("Heloo World \n");
+	// printf("Hi, Human \n");
+	printf("Hi, Computer \n");
+	
+	return 0;
+}
